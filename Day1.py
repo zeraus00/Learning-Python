068a435ba323f5c8bac4d6a7a68e4b2a93b836d7
zeraus00 <193580639+zeraus00@users.noreply.github.com>
2025-10-25 16:47:04 +0800

    Day 1

---
+++ Day1.py
@@ -0,0 +1,10 @@
+# Printing Text
+print("Hello 'Julius' world")
+print('Hello "Julius" world')
+print("Hello \"Julius\" world")
+print("Hello\nworld") # with break \n to print in the next line
+print("Julius", end = " ")# end " " prevent from new line
+print("Aboy", end = " ")
+print("Trinidad", end = " ")
+print() # print only a new line to ensure the cursor moves to the next line
+
