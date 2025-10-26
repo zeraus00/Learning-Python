b3f7d0f6014acd3326ca61376cb80e40ea5e5884
zeraus00 <193580639+zeraus00@users.noreply.github.com>
2025-10-26 15:52:05 +0800

    Day2

---
+++ Day2.py
@@ -0,0 +1,14 @@
+# day 2 of learning python
+
+# math library function
+
+import math
+
+pi = 3.14
+negative_pi = -3.14
+print(round(pi))
+print(math.ceil(pi))
+print(math.floor(pi))
+print(abs(negative_pi))
+print(pow(pi, 2))
+print(math.sqrt(420))
