d7d36460958a004cda7965995a56ebca111070a7
zeraus00 <193580639+zeraus00@users.noreply.github.com>
2025-10-26 16:32:07 +0800

    logical operators

--- Day2.py
+++ Day2.py
@@ -31,3 +31,38 @@ reversed_name = name[::-1]
 print(f"reverse name using [::-1], the output is: {reversed_name}\n")
 alt_reverse_name = ''.join(reversed(name))
 print(f"alternative reversed using ''.join(reversed(name)): {alt_reverse_name}\n")
+
+print("string list\n")
+names = ["julius", "April", "Nalyn", "Faith", "Carlo"]
+print(names[0])
+print(names[-2]) #negative index -3, -2, 1, start from the end element in the list
+
+# specifying index
+# note that end index is excluded
+names = ["julius", "April", "Nalyn", "Faith", "Carlo"]
+print(names)
+print(f"From 3 index to the last index: {names[3:]}\n")
+print(f"From the start to 2 index: {names[:3]}\n")
+print(f"From 1 index to 3: {names[1:4]}\n")
+print(f"From 1 index to 4 index: {names[:5]}")
+
+# use ', 'join(names[index]) to remove the [''] in output
+# sample
+print(f"Using ', '.join(names[index]) to remove the [''] in the output: {', '.join(names[1:3])}")
+
+# using slice function
+
+website1 = "http://google.com"
+website2 = "http://wikipedia.com"
+slice = slice(7, -4)
+print(website1[slice])
+print(website2[slice])
+
+# logical operators (and, or, not)
+
+temp = int(input("What is the temperature outside?: "))
+
+if temp >= 0 and temp <= 30:
+    print("\nthe temperature is good, go outside!")
+elif temp < 0 or temp > 30:
+    print("the temperature is bad today! stay inside!")
