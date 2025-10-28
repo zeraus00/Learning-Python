8cf07c30d22b671ac3c1d410e54a643d6a98ae8f
zeraus00 <193580639+zeraus00@users.noreply.github.com>
2025-10-28 10:10:43 +0800

    Temperature check done.

--- Day2Activity.py
+++ Day2Activity.py
@@ -13,6 +13,7 @@ while True:
         while True:
             age = input("Enter your age: ").strip()
             if age.isdigit():
+                age = int(age)
                 break
             else:
                 print("Please enter a valid age! (number).\n")
@@ -23,7 +24,7 @@ while True:
 
 print(f"Hello {name.upper()}! You are {age} years old.\n")
 
-if  18 <= int(age) < 65:
+if  18 <= age < 65:
     print("You are in the working age group.\n")
 elif age < 18:
     print("You are underage.\n")
@@ -31,3 +32,14 @@ else:
     print("You are senior citizen.\n")
 
 input("\nPress enter to continue...")
+
+# Temperature Check
+while True:
+    Temp = input("\nWhat is the Temperature outside (C)?: ").strip()
+    if Temp.lstrip('-').replace('.', '', 1).isdigit():
+        Message = "The weather is nice, you can go outside!" if 0 <= float(Temp) <= 30 else "Better stay inside, the weather is not ideal.\n" 
+        print(Message)
+        break
+    else:
+        print("Enter a valid input! (Number only).")
+print("Press Enter to continue...")
