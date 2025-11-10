0b426456eda2f0ba808c414bac60dba530bbcb5e
zeraus00 <193580639+zeraus00@users.noreply.github.com>
2025-11-10 19:49:25 +0800

    not finished

--- Day3.py
+++ Day3.py
@@ -1,1 +1,7 @@
 # Day 3 learning python
+# 2D list 
+drinks = ["coffee", "soda", "tea"]
+dinner = ["pizza", "hambuger", "hotdog"]
+dessert = ["cake", "ice cream"]
+
+food = [drinks, dinner, dessert]
